99d81ff56f74b0d41a7f12e32ee576e3e3f6416b
casinesque <marcocasini21@gmail.com>
2020-06-15 20:08:37 +0200

    My first commit

---
+++ Centos/script.sh
@@ -0,0 +1,24 @@
+echo "Starting script --------------------------------------------------------------"
+#This will be executed from inside the machine so the path are those of the guest machine.
+yum check update -y
+yum update -y
+#install httpd
+sudo yum install httpd -y
+#check httpd status
+sudo systemctl start httpd
+#check
+sudo su
+chkconfig httpd on
+systemctl status httpd | grep -i active
+#pwd 
+#cat /vagrant/proxy.conf
+machine_hostname=$(hostname) #Checking if the machine is the master
+if [ "$machine_hostname" = "master-node" ]; then
+    yes | rm /etc/httpd/conf.d/proxy.conf #remove current proxy conf configuration
+    cp /vagrant/proxy.conf /etc/httpd/conf.d/
+    echo "Proxy config has been copied" #The proxy conf must be set only on the master node.
+fi    
+cp /vagrant/index.html /var/www/html
+systemctl restart httpd.service #restart the httpd to take effects
+echo "Script executed, done-------------------------------------------------------"
+
